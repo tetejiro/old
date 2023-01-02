3f22357654c5128dab801aeea23eca5a78ed8428
tetejiro <hy1733505@gmail.com>
2023-01-02 23:25:53 +0900

    自動サーバ切り替え（DB）

--- DB.php
+++ DB.php
@@ -1,11 +1,11 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=portfolio2;charset=utf8';
-$user = 'yuki';
-$password = 'hy1733505';
-
-/*
-$dsn = 'mysql:host=mysql153.phy.lolipop.lan;dbname=LAA1321995-portfolio2;charset=utf8';
-$user = 'LAA1321995';
-$password = 'okikami07081103';
-*/
+if($_SERVER['HTTP_HOST'] == 'localhost') {
+  $dsn = 'mysql:host=localhost;dbname=portfolio2;charset=utf8';
+  $user = 'yuki';
+  $password = 'hy1733505';
+} else {
+  $dsn = 'mysql:host=mysql207.phy.lolipop.lan;dbname=LAA1452799-portfolio2;charset=utf8';
+  $user = 'LAA1452799';
+  $password = 'hy1733505';
+}
